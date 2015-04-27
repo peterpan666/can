8695ecb59e65d9cd92441939ac54fce71116ca1c
Adrien PEYROUTY <baboundu81@gmail.com>
2015-04-27 14:25:54 +0200

    Remove accents

--- code/lib.h
+++ code/lib.h
@@ -2,7 +2,7 @@
 #define _LIB_H_
 
 //Constantes
-//Période d'échantillonage en microseconde
+//Periode d'echantillonage en microseconde
 #define Te_us 10000
 
 //Macros
@@ -14,7 +14,7 @@
 //Convertie la base de temps (ms) pour coller avec le Te_us choisi
 #define conv_bdt(a) a*1000/Te_us
 
-//défini un nouveau type de données "bool" qui peut prendre FALSE ou TRUE
+//defini un nouveau type de donnees "bool" qui peut prendre FALSE ou TRUE
 typedef enum {FALSE = 0, TRUE = !FALSE} bool;
 
 void USART3_Start(void);
