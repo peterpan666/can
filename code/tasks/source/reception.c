5916d07f6f6560c48d0e40afcaf4be507d410b7b
Elie <faes.elie@gmail.com>
2015-06-05 16:43:44 +0200

    Add globals.h and reception state machine

--- code/tasks/source/reception.c
+++ code/tasks/source/reception.c
@@ -1,9 +1,11 @@
 #include "stm32f4xx.h"
 
 #include "lib.h"
+#include "globals.h"
 #include "reception.h"
 
 static uint8_t timer_reception = 0;
+static uint8_t state_reception = 0;
 
 void reception_timer(void) {
 	if(timer_reception != 0){
@@ -13,6 +15,13 @@ void reception_timer(void) {
 
 void reception_task(void) {
 	if(!timer_reception){
+		switch (state_reception) {
+			default:
+				state_reception = 0;
+			case 0:
+				break;
+
+		}
 		timer_reception = conv_bdt(2000);
 	}
 }
